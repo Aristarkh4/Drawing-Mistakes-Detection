f89abb9b73513e86e56bb3db347666de44b8f4bd
Aristarkh4 <Aristarkh4@gmail.com>
2017-07-11 01:17:22 +0300

    Add project files.

---
+++ Drawing Mistakes Detection/Drawing_Mistakes_Detection/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Drawing_Mistakes_Detection
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Drawing_Mistakes_Detection.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
