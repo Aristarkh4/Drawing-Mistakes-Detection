665ef530b2e34ba0d19bf937ae172a4822a1e264
Aristarkh4 <Aristarkh4@gmail.com>
2017-07-30 01:23:58 +1200

    Merge Image Analysis functionality and Azure Data Service classes.

---
+++ Drawing Mistakes Detection/Drawing_Mistakes_Detection/DrawingWithTag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing_Mistakes_Detection
+{
+    public class DrawingWithTag
+    {
+        [Newtonsoft.Json.JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.Version]
+        public string AzureVersion { get; set; }
+
+        public DateTime DateUtc { get; set; }
+        public byte TagId { get; set; }
+
+        [Newtonsoft.Json.JsonIgnore]
+        public string DateDisplay { get { return DateUtc.ToLocalTime().ToString("d"); } }
+
+        [Newtonsoft.Json.JsonIgnore]
+        public string TimeDisplay { get { return DateUtc.ToLocalTime().ToString("t"); } }
+    }
+}
