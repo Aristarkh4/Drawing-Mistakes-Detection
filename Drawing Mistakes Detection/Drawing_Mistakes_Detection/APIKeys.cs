95ab18e3254b9b77b5cb1053036dcdb99fc16323
Aristarkh4 <Aristarkh4@gmail.com>
2017-07-29 21:05:50 +1200

    Before-merge commit.

---
+++ Drawing Mistakes Detection/Drawing_Mistakes_Detection/APIKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace Drawing_Mistakes_Detection
+{
+    static class APIKeys
+    {
+        public static string PredictionKey = "4bd5d31bc7264aa0b359815c6e10ae85";
+        public static string PredictionURL = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/9fce6ada-e80d-48b0-88a8-44e29c26109f/image";
+    }
+}
